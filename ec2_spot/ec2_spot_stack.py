f03d89ff37f82bf2f8b875d18a461993d908c814
sebranchett <s.e.branchett@tudelft.nl>
2022-03-09 14:32:12 +0100

    added high level VPC with private subnet

--- ec2_spot/ec2_spot_stack.py
+++ ec2_spot/ec2_spot_stack.py
@@ -1,19 +1,23 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
 )
 from constructs import Construct
+
 
 class Ec2SpotStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "Ec2SpotQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # VPC
+        vpc_spot = ec2.Vpc(self, "VPC-spot",
+                           nat_gateways=0,
+                           subnet_configuration=[
+                               ec2.SubnetConfiguration(
+                                   name="private",
+                                   subnet_type=ec2.SubnetType.PRIVATE_ISOLATED
+                                   )
+                               ]
+                           )
