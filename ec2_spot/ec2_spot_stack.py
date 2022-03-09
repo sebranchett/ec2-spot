1053f745601f9bb6ae8258cb3c4bf50c2391499a
sebranchett <s.e.branchett@tudelft.nl>
2022-03-09 17:40:00 +0100

    ami not yet working

--- ec2_spot/ec2_spot_stack.py
+++ ec2_spot/ec2_spot_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    CfnTag,
     aws_ec2 as ec2,
 )
 from constructs import Construct
@@ -21,3 +22,43 @@ class Ec2SpotStack(Stack):
                                    )
                                ]
                            )
+
+        # Spot fleet
+        iamFleetRole = "arn:aws:iam::" + \
+                       self.account + \
+                       ":role/aws-ec2-spot-fleet-tagging-role"
+
+        # Deep Learning Base AMI (Ubuntami-02u 18.04) Version 50.0 in Frankfurt
+        my_ami = "ami-03ab7bf8ed4e51280"
+        my_instance_type = ec2.InstanceType("t2.small")
+        max_spot_price = "0.02"
+        # Always tag
+        my_tags = ec2.CfnSpotFleet.SpotFleetTagSpecificationProperty(
+            resource_type="resourceType",
+            tags=[CfnTag(key="Creator", value="SEB")]
+            )
+
+        # If you specify LaunchSpecifications,
+        # you can’t specify LaunchTemplateConfigs
+        launch_specs = ec2.CfnSpotFleet.SpotFleetLaunchSpecificationProperty(
+                        image_id=my_ami,
+                        )
+                        # instance_type=my_instance_type,
+                        # spot_price=max_spot_price,
+                        # tag_specifications=my_tags
+#                        user_data=
+#                        subnet_id=
+
+        config_data = ec2.CfnSpotFleet.SpotFleetRequestConfigDataProperty(
+                        iam_fleet_role=iamFleetRole,
+                        target_capacity=1,
+                        launch_specifications=launch_specs,
+                        on_demand_target_capacity=0,
+                        spot_price=max_spot_price,
+                        type="request"
+                        )
+
+        spot_fleet = ec2.CfnSpotFleet(
+                        self, "MyCfnSpotFleet",
+                        spot_fleet_request_config_data=config_data
+                        )
