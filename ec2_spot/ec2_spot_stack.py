18e4216a47a7f7add9319fdfb6f9a415ed5f635c
sebranchett <s.e.branchett@tudelft.nl>
2022-03-11 16:15:48 +0100

    very ugly, but leads to a spot instance

--- ec2_spot/ec2_spot_stack.py
+++ ec2_spot/ec2_spot_stack.py
@@ -1,4 +1,4 @@
-from base64 import b64encode, decode
+# from base64 import b64encode, decode
 from aws_cdk import (
     # Duration,
     Stack,
@@ -11,15 +11,15 @@ from constructs import Construct
 # amazon/Deep Learning Base AMI (Amazon Linux) Version 31.1
 # in Frankfurt, found using:
 # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html#finding-an-ami-aws-cli
-my_ami = "ami-001785e22bc8fd37c"
+my_ami = "ami-03ab7bf8ed4e51280"
 my_instance_type = "t2.small"
 my_max_spot_price = "0.02"
 my_tags = [ec2.CfnSpotFleet.SpotFleetTagSpecificationProperty(
     resource_type="instance",
     tags=[CfnTag(key="Creator", value="SEB")]
     )]
-byte_data = open("./user_data/user_data.sh", "rb").read()
-my_user_data = b64encode(byte_data).decode()
+# byte_data = open("./user_data/user_data.sh", "rb").read()
+# my_user_data = b64encode(byte_data).decode()
 
 
 class Ec2SpotStack(Stack):
@@ -32,34 +32,63 @@ class Ec2SpotStack(Stack):
                        ":role/aws-ec2-spot-fleet-tagging-role"
 
         # VPC
-        ec2.Vpc(self, "VPC-spot",
-                nat_gateways=0,
-                subnet_configuration=[
-                    ec2.SubnetConfiguration(
-                        name="private",
-                        subnet_type=ec2.SubnetType.PRIVATE_ISOLATED
-                        )
-                    ]
-                )
+        my_vpc = ec2.Vpc(self, "VPC-spot",
+                         nat_gateways=0,
+                         subnet_configuration=[
+                             ec2.SubnetConfiguration(
+                                 name="private",
+                                 subnet_type=ec2.SubnetType.PRIVATE_ISOLATED
+                                 )
+                             ]
+                         )
+        my_subnets = my_vpc.select_subnets().subnet_ids
+        print(my_subnets)
         # Spot fleet
         # If you specify LaunchSpecifications,
         # you can’t specify LaunchTemplateConfigs
-        launch_specs = [ec2.CfnSpotFleet.SpotFleetLaunchSpecificationProperty(
-                        image_id=my_ami,
-                        instance_type=my_instance_type,
-                        spot_price=my_max_spot_price,
-                        tag_specifications=my_tags,
-                        user_data=my_user_data
-                        )]
+        # launch_specs = \
+        #     [ec2.CfnSpotFleet.SpotFleetLaunchSpecificationProperty(
+        #                 image_id="ami-03ab7bf8ed4e51280",
+        #                 instance_type="t2.medium",
+        #                 ),
+        #                 ec2.CfnSpotFleet.SpotFleetLaunchSpecificationProperty(
+        #                 image_id="ami-03ab7bf8ed4e51280",
+        #                 instance_type="t2.small",
+        #                 ),
+        #                 ec2.CfnSpotFleet.SpotFleetLaunchSpecificationProperty(
+        #                 image_id="ami-03ab7bf8ed4e51280",
+        #                 instance_type="m3.medium",
+        #                 ),
+        #                 ]
+        #                 # tag_specifications=my_tags,
+        #                 # user_data=my_user_data
 
-        config_data = ec2.CfnSpotFleet.SpotFleetRequestConfigDataProperty(
-                        iam_fleet_role=iamFleetRole,
-                        target_capacity=1,
-                        launch_specifications=launch_specs,
-                        on_demand_target_capacity=0,
-                        spot_price=my_max_spot_price,
-                        type="request"
-                        )
+        config_data = \
+            ec2.CfnSpotFleet.SpotFleetRequestConfigDataProperty(
+                allocation_strategy="capacityOptimized",
+                iam_fleet_role=iamFleetRole,
+                spot_price="0.08",
+                target_capacity=1,
+                terminate_instances_with_expiration=True,
+                type="request",
+                launch_specifications=[
+                    ec2.CfnSpotFleet.SpotFleetLaunchSpecificationProperty(
+                        image_id="ami-03ab7bf8ed4e51280",
+                        instance_type="t2.medium",
+                        subnet_id=my_subnets[0],
+                    ),
+                    ec2.CfnSpotFleet.SpotFleetLaunchSpecificationProperty(
+                        image_id="ami-03ab7bf8ed4e51280",
+                        instance_type="t2.small",
+                        subnet_id=my_subnets[0],
+                    ),
+                    ec2.CfnSpotFleet.SpotFleetLaunchSpecificationProperty(
+                        image_id="ami-03ab7bf8ed4e51280",
+                        instance_type="m3.medium",
+                        subnet_id=my_subnets[0],
+                    ),
+                    ]
+                )
 
         ec2.CfnSpotFleet(
                         self, "MyCfnSpotFleet",
