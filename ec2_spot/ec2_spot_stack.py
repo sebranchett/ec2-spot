f5615f6dbb70c8d8213e1f3ed060e3254ba90550
sebranchett <s.e.branchett@tudelft.nl>
2022-03-11 17:37:01 +0100

    added user data to instance

--- ec2_spot/ec2_spot_stack.py
+++ ec2_spot/ec2_spot_stack.py
@@ -1,4 +1,4 @@
-# from base64 import b64encode, decode
+from base64 import b64encode, decode
 from aws_cdk import (
     # Duration,
     Stack,
@@ -13,13 +13,13 @@ from constructs import Construct
 # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html#finding-an-ami-aws-cli
 my_ami = "ami-03ab7bf8ed4e51280"
 my_instance_types = ["t2.small", "t2.medium", "m3.medium"]
-my_max_spot_price = "0.02"
+my_max_spot_price = "0.08"
 my_tags = [ec2.CfnSpotFleet.SpotFleetTagSpecificationProperty(
     resource_type="instance",
     tags=[CfnTag(key="Creator", value="SEB")]
     )]
-# byte_data = open("./user_data/user_data.sh", "rb").read()
-# my_user_data = b64encode(byte_data).decode()
+byte_data = open("./user_data/user_data.sh", "rb").read()
+my_user_data = b64encode(byte_data).decode()
 
 
 class Ec2SpotStack(Stack):
@@ -51,6 +51,7 @@ class Ec2SpotStack(Stack):
                         image_id=my_ami,
                         instance_type=inst_type,
                         subnet_id=subnet,
+                        user_data=my_user_data,
                         )
                     )
 
@@ -59,7 +60,7 @@ class Ec2SpotStack(Stack):
             ec2.CfnSpotFleet.SpotFleetRequestConfigDataProperty(
                 allocation_strategy="capacityOptimized",
                 iam_fleet_role=iamFleetRole,
-                spot_price="0.08",
+                spot_price=my_max_spot_price,
                 target_capacity=1,
                 terminate_instances_with_expiration=True,
                 type="request",
--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo yum update -y
-sudo docker pull sebranchett/gpaw:one_processor_21.1.0
-sudo docker run --rm sebranchett/gpaw:one_processor_21.1.0 gpaw test
+echo -----------------------------------
+echo Hello, this is a message from Susan
+echo -----------------------------------
+sudo shutdown -h now
